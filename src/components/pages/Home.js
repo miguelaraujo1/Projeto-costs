afbb1d035bd6f5243a8de82226c482ebe1e8eace
Miguel Araujo <miguelaraujo2002@gmail.com>
2023-06-20 20:05:48 -0300

    Criação css formulário

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,12 +4,12 @@ import LinkButton from '../layout/LinkButton'
 
 function Home() {
     return (
+        
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar seus projetos agora mesmo</p>
             <LinkButton to="/newproject" text="Criar Projeto"/>
             <img src={savings} alt="Costs"/>
-
         </section>
     )
 }
